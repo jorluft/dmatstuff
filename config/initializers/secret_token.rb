ac1ae0faa459323841537b58481ffd1c44ba2471
Jordan Luft <Jorluft@gmail.com>
2014-01-31 11:11:47 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dmatstuff::Application.config.secret_key_base = '41696f4f2ec8596ac4da0f1075bd5607e3284cde00dd9eb1ab9c77812b3fc4cf795d619e3c1da670f21ffb8d0b9da46f42b18e43a1d525cf85707d3b9dc09965'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dmatstuff::Application.config.session_store :cookie_store, key: '_dmatstuff_session'
